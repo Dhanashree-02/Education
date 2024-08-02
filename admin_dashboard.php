78da5e1499d311311c787b07cbafe2e98f2f5567
Dhanashree-02 <dhanashree.sonune02@gmail.com>
2024-08-02 12:17:42 -1000

    Admin

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -15,41 +15,8 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     
-    <link rel = "stylesheet" href = "admin_panel/admin_panel.css">
-    <style>
-        .card {
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            border-radius: 15px;
-            padding: 5px;
-            margin: 5px;
-            background-color: #fff;
-            display: inline-block;
-            width: calc(33% - 20px);
-            text-align: center;
-            position: relative;
-            transition: transform 0.5s ease;
-            border: 1px solid black;
-        }
-        .card:hover {
-            transform: translateY(-5px);
-        }
-        .card .view-button {
-            position: absolute;
-            bottom: 10px;
-            right: 10px;
-            background-color: #007bff;
-            color: white;
-            border: none;
-            padding: 10px 10px;
-            border-radius: 15px;
-            cursor: pointer;
-            text-decoration: none;
-            transition: background-color 0.5s ease;
-        }
-        .card .view-button:hover {
-            background-color: #0056b3;
-        }
-    </style>
+    <link rel = "stylesheet" href = "admin_panel.css">
+
 </head>
 <body>
 
@@ -67,18 +34,18 @@ if (!isset($_SESSION['username'])) {
     <p>This is your admin dashboard.</p>
     <div class="card">
         <h3>Universities</h3>
-        <p>Manage universities details.</p>
+        <p>Universities details.</p>
         <a href="admin_panel/universities.php" class="view-button">View</a>
     </div>
     <div class="card">
         <h3>Courses</h3>
-        <p>Manage courses details.</p>
+        <p>Courses details.</p>
         <a href="admin_panel/courses.php" class="view-button">View</a>
     </div>
 
     <div class="card">
         <h3>Contact</h3>
-        <p>Manage contact information.</p>
+        <p>Contact information.</p>
         <a href="admin_panel/Contact.php" class="view-button">View</a>
     </div>
 
